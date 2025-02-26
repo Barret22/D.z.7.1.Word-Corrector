4397a11c0614cfc3cc02e15935c19c7a23896378
Barret22 <vviktoroduvan@gmail.com>
2025-02-26 19:57:41 +0100

    we create a project

--- app/Corrector.java
+++ app/Corrector.java
@@ -1,4 +1,26 @@
 package app;
 
 public class Corrector {
+
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+
+        for (String str : strs) {
+            // Перевіряємо, чи містить слово літеру 'b', і якщо так — замінюємо на 'o'
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+
+            // Збільшуємо лічильник та додаємо до рядка виведення
+            count++;
+            stringBuilder
+                    .append(count)
+                    .append(") ")
+                    .append(str)
+                    .append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
--- app/Main.java
+++ app/Main.java
@@ -1,4 +1,20 @@
 package app;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        // Отримуємо масив слів із DataProvider
+        String[] data = new DataProvider().getData();
+
+        // Створюємо екземпляр Corrector та передаємо масив слів на обробку
+        String output = new Corrector().handleData(data);
+
+        // Виводимо результат у консоль
+        getOutput(output);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
